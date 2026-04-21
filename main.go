d6292885f613f01c2a66e75a8df2655b94474fb2
Miguel Ortega <miguel.ortega@seamware.com>
2026-04-21 09:28:18 +0200

    refactor main.go

--- main.go
+++ main.go
@@ -18,110 +18,119 @@ func init() {
 	zap.ReplaceGlobals(zap.Must(zap.NewDevelopment()))
 }
 
-func getHostPath(hostUrl string) string {
+func getHostPath(hostUrl string) (string, error) {
 	webUrl, err := url.Parse(hostUrl)
 	if err != nil {
-		zap.L().Sugar().Errorf("'%s' is not a valid url", hostUrl)
-		os.Exit(7)
+		return "", fmt.Errorf("'%s' is not a valid url", hostUrl)
 	}
-	return webUrl.Path
+	return webUrl.Path, nil
 }
 
-func main() {
-	var cfg did.Config
-	var fileContent []byte
-	var resultingDid string
-	var err error
-
-	filler := flagsfiller.New(flagsfiller.WithEnv(""))
-	err = filler.Fill(flag.CommandLine, &cfg)
-	if err != nil {
-		zap.L().Sugar().Fatal("error reading config. error %s", err)
-		os.Exit(1)
-	}
-	flag.Parse()
-
-	if cfg.DidType != "keycloak" {
-		err = did.LoadCertificates(&cfg)
-		if err != nil {
-			os.Exit(1)
-		}
-	}
+func resolveDID(cfg did.Config) (string, error) {
 	switch cfg.DidType {
 	case "key":
-		resultingDid, err = did.GetDIDKey(cfg)
+		return did.GetDIDKey(cfg)
 	case "jwk":
-		resultingDid, err = did.GetDIDJWKFromKey(cfg)
+		return did.GetDIDJWKFromKey(cfg)
 	case "web":
-		resultingDid, err = did.GetDIDWeb(cfg.HostUrl)
+		return did.GetDIDWeb(cfg.HostUrl)
 	case "keycloak":
 		if cfg.KeycloakRealm != "" {
-			resultingDid, err = did.GetDIDWeb(cfg.HostUrl)
+			return did.GetDIDWeb(cfg.HostUrl)
 		}
+		return "", nil
 	default:
-		zap.L().Sugar().Warnf("Did type %s is not supported.", cfg.DidType)
-		os.Exit(2)
+		return "", fmt.Errorf("did type %s is not supported", cfg.DidType)
 	}
+}
 
-	if err != nil {
-		fmt.Println("Was not able to extract did. Err: ", err)
-		os.Exit(3)
-	} else if resultingDid != "" {
-		fmt.Println("Did key is: ", resultingDid)
-	}
-
+func buildOutput(cfg *did.Config, resultingDid string) ([]byte, error) {
 	switch cfg.OutputFormat {
 	case "json":
 		didJson := did.Did{IssuerDid: []string{"https://www.w3.org/ns/did/v1"}, Id: resultingDid}
-		fileContent, err = json.Marshal(didJson)
-		if err != nil {
-			zap.L().Sugar().Warnf("Was not able to marshal the did-json. Err: %s", err)
-			os.Exit(4)
-		}
+		return json.Marshal(didJson)
 	case "env":
-		fileContent = ([]byte("DID=" + resultingDid))
+		return []byte("DID=" + resultingDid), nil
 	case "json_jwk":
 		if cfg.CertUrl == "" {
 			cfg.CertUrl = strings.TrimSuffix(cfg.HostUrl, "/") + "/.well-known/tls.crt"
 		}
-		keySet, err := did.GenerateJWK(cfg)
+		keySet, err := did.GenerateJWK(*cfg)
 		if err != nil {
-			zap.L().Sugar().Warnf("Error generating keyset. Err: %s", err)
-			os.Exit(5)
+			return nil, fmt.Errorf("error generating keyset: %w", err)
 		}
 		verificationMethod := did.VerificationMethod{Id: resultingDid, Type: "JsonWebKey2020", Controller: resultingDid, PublicKeyJwk: keySet}
 		didJson := did.Did{Context: []string{"https://www.w3.org/ns/did/v1"}, Id: resultingDid, VerificationMethod: []did.VerificationMethod{verificationMethod}}
-		fileContent, err = json.MarshalIndent(didJson, "", "  ")
-		if err != nil {
-			zap.L().Sugar().Warnf("Error printing keyset")
-			os.Exit(6)
+		return json.MarshalIndent(didJson, "", "  ")
+	}
+	return nil, nil
+}
+
+func startServer(cfg did.Config, fileContent []byte, resultingDid string) error {
+	if cfg.DidType == "keycloak" {
+		var basepath string
+		if cfg.KeycloakRealm != "" {
+			var err error
+			basepath, err = getHostPath(cfg.HostUrl)
+			if err != nil {
+				return err
+			}
+		}
+		server := did.NewKeycloakServer(cfg.KeycloakHost, cfg.ServerPort, cfg.IgnoreTlsValidation, cfg.KeycloakRealm, resultingDid, basepath)
+		return server.Start()
+	}
+
+	cert, _ := did.GetCert(cfg)
+	didFilename := "did.json"
+	if cfg.OutputFormat == "env" {
+		didFilename = "did.env"
+	}
+	hostPath, err := getHostPath(cfg.HostUrl)
+	if err != nil {
+		return err
+	}
+	server := did.NewDidServer(string(fileContent), string(cert), cfg.ServerPort, hostPath, didFilename)
+	return server.Start()
+}
+
+func main() {
+	var cfg did.Config
+
+	filler := flagsfiller.New(flagsfiller.WithEnv(""))
+	if err := filler.Fill(flag.CommandLine, &cfg); err != nil {
+		zap.L().Sugar().Fatalf("error reading config: %s", err)
+	}
+	flag.Parse()
+
+	if cfg.DidType != "keycloak" {
+		if err := did.LoadCertificates(&cfg); err != nil {
+			os.Exit(1)
 		}
 	}
-	if cfg.OutputFile != "" {
 
-		err = os.WriteFile(cfg.OutputFile, fileContent, 0644)
-		if err != nil {
-			zap.L().Sugar().Warnf("Was not able to write the did-json to %s. Err: %s", cfg.OutputFile, err)
-			os.Exit(7)
+	resultingDid, err := resolveDID(cfg)
+	if err != nil {
+		zap.L().Sugar().Fatalf("was not able to resolve did: %s", err)
+	}
+	if resultingDid != "" {
+		fmt.Println("Did key is: ", resultingDid)
+	}
+
+	fileContent, err := buildOutput(&cfg, resultingDid)
+	if err != nil {
+		zap.L().Sugar().Fatalf("was not able to build output: %s", err)
+	}
+
+	switch {
+	case cfg.OutputFile != "":
+		if err := os.WriteFile(cfg.OutputFile, fileContent, 0644); err != nil {
+			zap.L().Sugar().Fatalf("was not able to write to %s: %s", cfg.OutputFile, err)
 		}
-	} else if cfg.RunServer {
-		if cfg.DidType == "keycloak" {
-			var basepath string
-			if cfg.KeycloakRealm != "" {
-				basepath = getHostPath(cfg.HostUrl)
-			}
-			server := did.NewKeycloakServer(cfg.KeycloakHost, cfg.ServerPort, cfg.IgnoreTlsValidation, cfg.KeycloakRealm, resultingDid, basepath)
-			server.BaseServer.Start()
-		} else {
-			cert, _ := did.GetCert(cfg)
-			didFilename := "did.json"
-			if cfg.OutputFormat == "env" {
-				didFilename = "did.env"
-			}
-			server := did.NewDidServer(string(fileContent), string(cert), cfg.ServerPort, getHostPath(cfg.HostUrl), didFilename)
-			server.Start()
+	case cfg.RunServer:
+		if err := startServer(cfg, fileContent, resultingDid); err != nil {
+			zap.L().Sugar().Fatalf("server error: %s", err)
 		}
-	} else {
+	default:
 		fmt.Println("Output: ", string(fileContent))
 	}
 }
